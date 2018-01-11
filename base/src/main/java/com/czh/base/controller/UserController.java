7be88d5f797907b7cdb2cdbe7f55c586f22a4c79
ayusli127 <ayusli127@gmail.com>
2018-01-11 17:40:18 +0800

    first commit

---
+++ base/src/main/java/com/czh/base/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.czh.base.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
